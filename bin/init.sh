e2bdacfa1443f4b1f5ef23d11ee4f220d5372557
Lennart Kats <lclkats@gmail.com>
2013-09-11 11:30:25 +0200

    Revert "Merge pull request #1 from c9/chat-demo"
    This reverts commit efbfde1ad824585bfb4f98e3c51ec0e87886b4cf, reversing
    changes made to d1d733844af3c9e1c3fae998f1be94214d265ba3.

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,1 @@
-cd ~/$C9_PID
-cp -rf /usr/libexec/openshift/cartridges/c9-0.1/root/templates/nodejs-chat/* .
-rm -f ~/bin/init.sh ~/bin/merge-to-other-templates.sh
+rm -f bin/init.sh bin/merge-to-other-templates.sh
