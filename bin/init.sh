b4c269fc3b8382808a15c51f5d8effdb25b08820
Fabian Jakobs <fabian@c9.io>
2014-02-24 15:03:11 +0100

    adapt to docker

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,3 @@
 cd ~/$C9_PID
-cp -rf /usr/libexec/openshift/cartridges/c9-0.1/root/templates/nodejs-chat/* .
+cp -rf /mnt/shared/templates/nodejs-chat/* .
 rm -f ~/bin/init.sh ~/bin/merge-to-other-templates.sh
